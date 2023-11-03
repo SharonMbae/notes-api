f3e8e5b45c6e1865849a9ea3b8eb3df39d026222
SharonMba <mbaesharon@gmail.com>
2023-11-03 06:50:41 -0700

    notes-api

--- src/notes.ts
+++ src/notes.ts
@@ -1,5 +1,4 @@
 import express, { Request, Response, json, request, response } from 'express'
-const {}= require('mysql')
 
 const app=express()
 
@@ -21,6 +20,60 @@ let allnotes=[
     {noteId: 2 , noteTitle: 'Instructions', noteContent: 'Dont leave without signing out '}
 ]
 
+app.post('/allnotes', (req: Request, res: Response)=>{
+    const note1= req.body;
+    res.status(201).json(note1);
+});
+
+app.get('/allnotes', (req:Request, res:Response)=>{
+    res.status(200).json(allnotes);
+});
+
+app.get('/allnotes',(req:Request, res:Response)=>{
+    const noteId = parseInt(req.params.id);
+  const note = allnotes.find((n) => n.noteId === noteId);
+  if (!note) {
+    res.status(404).json({ message: 'Note does not exist' });
+  } else {
+    res.status(200).json(note);
+  }
+
+});
+
+app.put('/allnotes/:id', (req:Request, res:Response) => {
+  const noteId = parseInt(req.params.id);
+  const updatedNote = req.body;
+
+  const index = allnotes.findIndex((n) => n.noteId === noteId);
+  if (index === -1) {
+    res.status(404).json({ message: 'Note does not exist' });
+  } else {
+    allnotes[index] = updatedNote;
+    res.status(200).json(updatedNote);
+  }
+});
+
+app.delete('/notes/:id', (req, res) => {
+      const noteId = parseInt(req.params.id);
+      const index = allnotes.findIndex((n) => n.noteId === noteId);
+      if (index === -1) {
+        res.status(404).json({ message: 'Note does not exist' });
+      } else {
+        allnotes.splice(index, 1);
+        res.status(204).end(); 
+      }
+    });
+
+    app.delete('/notes/:id', (req: Request, res:Response) => {
+          const noteId = parseInt(req.params.id);
+          const index = allnotes.findIndex((n) => n.noteId === noteId);
+          if (index === -1) {
+            res.status(404).json({ message: 'Could not find the note' });
+          } else {
+            allnotes.splice(index, 1);
+            res.status(204).end(); 
+          }
+        });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
